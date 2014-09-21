155085e7d3713eedfeaf2c4102d505e92bbb54e2
Mitch Downey <mitchldowney@gmail.com>
2014-09-21 17:07:10 -0500

    Initial commit

---
+++ js/base.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $("#xs-navbar-show-search").click(function() {
+        $("#navbar-brand").toggle("fast");
+        $("#ev-navbar-brand").toggle("fast");
+        $("#xs-navbar-search").toggle("fast");
+
+        if ( $("#xs-navbar-show-search").html() === '<i class="fa fa-close"></i>') {
+            $("#xs-navbar-show-search").html("<i class='fa fa-search'></i>");
+        } else {
+            $("#xs-navbar-show-search").html("<i class='fa fa-close'></i>");
+        }
+    });
+    $("#folder-1-bar").click(function() {
+        if ( $("#folder-1-icon").attr('class') === 'fa fa-caret-right caret-width') {
+            $("#folder-1-icon").attr('class', 'fa fa-caret-down caret-width');
+            $("#folder-1").toggle();
+        } else {
+            $("#folder-1-icon").attr('class', 'fa fa-caret-right caret-width');
+            $("#folder-1").toggle();
+        }
+    });
+    $("#folder-1-1-bar").click(function() {
+        if ( $("#folder-1-1-icon").attr('class') === 'fa fa-caret-right caret-width') {
+            $("#folder-1-1-icon").attr('class', 'fa fa-caret-down caret-width');
+            $("#folder-1-1").toggle();
+        } else {
+            $("#folder-1-1-icon").attr('class', 'fa fa-caret-right caret-width');
+            $("#folder-1-1").toggle();
+        }
+    });
+});
